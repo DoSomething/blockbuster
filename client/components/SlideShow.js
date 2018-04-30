b12ed21535c094d9d2ebacb91fcb2abaeece6df9
Mendel Blesofsky <mendelblesofsky@gmail.com>
2018-04-30 09:51:59 -0400

    Add SlideShow component

---
+++ client/components/SlideShow.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+import '../styles/Screen.scss';
+
+class SlideShow extends React.Component {
+  constructor(props) {
+    super(props) 
+
+    this.state = {
+      slide: 0,
+    }
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(() => {
+      let slide = this.state.slide+1;
+      if (slide >= this.props.slides.length) {
+        slide = 0;
+      }
+      this.setState({slide});
+    }, 5000)
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  render() {
+    const slide = this.props.slides[this.state.slide];
+    const src = slide.background;
+
+    return (
+      <img src={src}/>
+    );
+  }
+}
+
+export default SlideShow;
